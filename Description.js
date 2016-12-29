4866fb3b96516178b6bcea9c21c491cbd9c13201
Dixit Patel <talkwithdixit@gmail.com>
2016-12-28 22:54:08 -0500

    Added less file

--- Description.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-const style = require('./style.less');
-export class Description extends React.Component {
-    render() {
-        return (<p className={style.description}>
-        {this.props.children}
-      </p>);
-    }
-}
