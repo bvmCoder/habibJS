1aad35ea2a4e3e77f8558d1f843d9d8a2c90a040
@bvmCoder <talkwithdixit@gmail.com>
2016-12-28 22:36:25 -0500

    Add files via upload

---
+++ demo/download/Download.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Header } from './../header/Header';
+export class Download extends React.Component {
+    render() {
+        return (<div>
+        <Header activePage='Donwload'/>
+        <p>Download</p>
+      </div>);
+    }
+}
