f781210ecfb1d5c136165992b1a1ab48f4c1d457
@bvmCoder <talkwithdixit@gmail.com>
2017-02-21 05:09:40 -0500

    Final Solution

---
+++ src/data.js
@@ -0,0 +1,50 @@
+[
+  {
+    "text": "No Coupon",
+    "value": "--",
+    "key": 2,
+    "selected": true
+  },
+  {
+    "text": "FLT",
+    "value": "FLT",
+    "key": 5,
+    "selected": true
+  },
+  {
+    "text": 1.85,
+    "value": 1.85,
+    "key": 13,
+    "selected": true
+  },
+  {
+    "text": 2.18,
+    "value": 2.18,
+    "key": 11,
+    "selected": true
+  },
+  {
+    "text": 7.375,
+    "value": 7.375,
+    "key": 4,
+    "selected": true
+  },
+  {
+    "text": 8.5,
+    "value": 8.5,
+    "key": 19,
+    "selected": true
+  },
+  {
+    "text": 8.75,
+    "value": 8.75,
+    "key": 10,
+    "selected": true
+  },
+  {
+    "text": 10.375,
+    "value": 10.375,
+    "key": 1,
+    "selected": true
+  }
+]
