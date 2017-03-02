c1d5c3e1c50f678a008c1a6d9292da0b0fa19764
@bvmCoder <talkwithdixit@gmail.com>
2017-03-02 15:23:46 -0500

    get unique Object from the Original Array

---
+++ src/ArrayOfObjectsUnique.js
@@ -0,0 +1,117 @@
+(function() {
+    'use strict';
+    let coupons = [{
+        "text": 10.375,
+        "value": 10.375,
+        "key": 1,
+        "selected": true
+    }, {
+        "text": "No Coupon",
+        "value": "--",
+        "key": 2,
+        "selected": true
+    }, {
+        "text": "No Coupon",
+        "value": "--",
+        "key": 3,
+        "selected": true
+    }, {
+        "text": 7.375,
+        "value": 7.375,
+        "key": 4,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 5,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 6,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 7,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 8,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 9,
+        "selected": true
+    }, {
+        "text": 8.75,
+        "value": 8.75,
+        "key": 10,
+        "selected": true
+    }, {
+        "text": 2.18,
+        "value": 2.18,
+        "key": 11,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 12,
+        "selected": true
+    }, {
+        "text": 1.85,
+        "value": 1.85,
+        "key": 13,
+        "selected": true
+    }, {
+        "text": 1.85,
+        "value": 1.85,
+        "key": 14,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 15,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 16,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 17,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 18,
+        "selected": true
+    }, {
+        "text": 8.5,
+        "value": 8.5,
+        "key": 19,
+        "selected": true
+    }, {
+        "text": "No Coupon",
+        "value": "--",
+        "key": 20,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 21,
+        "selected": true
+    }, {
+        "text": "FLT",
+        "value": "FLT",
+        "key": 22,
+        "selected": true
+    }];
+    const dedupThings =Array.from(coupons.reduce((m, t) => m.set(t.text, t), new Map()).values());
+    console.log(dedupThings);
+
+})();
